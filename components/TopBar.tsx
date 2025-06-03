9f89897787bf0346b0636f279098745be4278b85
rip4ik14 <rip4inskiy@yandex.ru>
2025-06-03 14:41:37 +0300

    описание изменений, например: fix: исправил generateStaticParams

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -5,16 +5,16 @@ export default function TopBar() {
   const textItems = [
     '• Фото букета перед доставкой •',
     '• Доставка от 2ч •',
-    '•  Клубника в шоколаде и авторские букеты •',
+    '• Гарантия на цветы 3 дня •',
     '• Кешбэк с каждого заказа до 15% •',
   ];
 
   return (
     <div className="bg-black text-white overflow-hidden">
-      <div className="max-w-7xl mx-auto px-2">
+      <div className="max-w-5xl mx-auto px-1">
         <div className="relative">
           <div
-            className="animate-сflex w-max whitespace-nowrap text-sm py-2"
+            className="animate-marquee flex w-max whitespace-nowrap text-sm py-2"
             role="marquee"
             aria-label="Информационная панель"
           >
