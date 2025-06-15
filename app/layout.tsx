de76678f6e5e92cf4714379e8345ae8806528c68
rip4ik14 <rip4inskiy@yandex.ru>
2025-06-15 18:10:59 +0300

    описание изменений, например: fix: исправил generateStaticParams

--- app/layout.tsx
+++ app/layout.tsx
@@ -187,7 +187,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         {ymId && (
           <Script id="ym" strategy="lazyOnload">
             {`
-              (function(m,e,t,r,i,k,a){
+                (function(m,e,t,r,i,k,a){
                 m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                 m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0];
                 k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
