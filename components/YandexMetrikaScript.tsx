96e0cfd0e9ae0da1ee5d9ca51bed87ae0241c603
rip4ik14 <48773688+rip4ik14@users.noreply.github.com>
2025-06-18 11:20:29 +0200

    refactor: move yandex metrika script

---
+++ components/YandexMetrikaScript.tsx
@@ -0,0 +1,33 @@
+// ✅ Путь: components/YandexMetrikaScript.tsx
+'use client';
+
+import Script from 'next/script';
+
+interface YandexMetrikaScriptProps {
+  ymId: number;
+}
+
+export default function YandexMetrikaScript({ ymId }: YandexMetrikaScriptProps) {
+  return (
+    <Script
+      id="ym"
+      data-nosnippet
+      strategy="afterInteractive"
+      src="https://mc.yandex.ru/metrika/tag.js"
+      onLoad={() => {
+        if (typeof window.ym === 'function') {
+          // @ts-ignore: window.ym accepts object init params
+          window.ym(ymId, 'init', {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            trackHash: true,
+            webvisor: true,
+          });
+        } else {
+          console.error('Yandex Metrika is not loaded');
+        }
+      }}
+    />
+  );
+}
