b325dfd77c7fd1edfa5a127275ea85bb32418f23
rip4ik14 <rip4inskiy@yandex.ru>
2025-07-14 10:48:02 +0300

    описание изменений, например: fix: исправил generateStaticParams

--- components/CategoryPreviewServer.tsx
+++ components/CategoryPreviewServer.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import ProductCard from './ProductCard';
 import type { Product } from '@/types/product';
-import { claimPriority } from '@/types/imagePriority';
+import { claimPriority } from '@/utils/imagePriority';
 
 interface Props {
   categoryName: string;
