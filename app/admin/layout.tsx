960f9fd03b21792e1d73786bc197df245cbc0916
rip4ik14 <rip4inskiy@yandex.ru>
2025-12-15 22:30:32 +0300

    описание изменений, например: fix: исправил generateStaticParams

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,9 +1,8 @@
-import { Toaster } from 'react-hot-toast';
-import type { ReactNode } from 'react';
+import { Toaster } from "react-hot-toast";
+import type { ReactNode } from "react";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
